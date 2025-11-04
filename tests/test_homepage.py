6c674fd5ed7ef6a64d2eebb2edc73244be275d37
rowlandfas <ayodele.rowland@cloudhight.com>
2025-11-04 13:55:01 +0100

    Update test_homepage.py

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,1 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+def test_homepage_loads():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://15.237.28.123:8080")
+
+    assert "Pet Adoption" in driver.title
+    driver.quit()
 
